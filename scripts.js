cd2458cf140a17ea1b25713b73b05aece38c9cc6
Jarek Gajdulewicz <j.gajdulewicz@icloud.com>
2018-01-25 14:08:41 +0100

    Add html & js

---
+++ scripts.js
@@ -0,0 +1,10 @@
+// scripts.js
+
+var name = prompt('Enter your name');
+//alert('Hello, ' + name);
+console.log('Hello, ' + name);
+/*
+  block comment
+  console.log('The commented will not be called');
+*/
+//line comment - this line does not exist
